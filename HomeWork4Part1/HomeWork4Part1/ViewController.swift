1cfe795810e6b963feda5165748a0c99c5d72fb2
Bohdan <borysenkobohdan@gmail.com>
2023-06-16 23:09:46 +0300

    firstApp

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var titelLabel: UILabel!
+    
+    
+    @IBAction func titleButton(_ sender: Any) {
+        titelLabel.text = "Welcome is my app"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
+
 
